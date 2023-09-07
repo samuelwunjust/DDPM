87f0a938c63d32f1622e14354bfb51b6f8769bda
Samuel Ng <107619167+samuelwunjust@users.noreply.github.com>
2023-09-07 17:21:11 +0800

    Update utils.py

--- DDPM/Diffusion/utils.py
+++ DDPM/Diffusion/utils.py
@@ -7,9 +7,10 @@ from torch.utils.data import DataLoader
 
 
 def plot_images(images):
-    plt.figure(figsize=(32,32))
-    plt.imshow(torch.cat(torch.cat([i for i in images.cpu()],dim=-1),dim=-2).permute(1,2,0).cpu())
+    plt.figure(figsize=(32, 32))
+    plt.imshow(torch.cat([torch.cat([i for i in images.cpu()], dim=-1),], dim=-2).permute(1, 2, 0).cpu())
     plt.show()
+
 
 
 def save_images(image,path,**kwargs):
@@ -21,7 +22,7 @@ def save_images(image,path,**kwargs):
 def get_data(args):
     transform=torchvision.transforms.Compose(
            [torchvision.transforms.Resize(80),
-            torchvision.transforms.RandomResizedCrop(args.image.size(),scale=(0.8,1)),
+            torchvision.transforms.RandomResizedCrop(args.image_size,scale=(0.8,1)),
             torchvision.transforms.ToTensor(),
             torchvision.transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))]
 
